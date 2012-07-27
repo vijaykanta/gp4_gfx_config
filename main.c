6d186e970117ec63b5462c68a8f6c7d30078bb97
Vijay Kanta <antimonyxy@msn.com>
2012-07-27 08:17:15 +0530

    More additions of code, tweaks, function to save original backup. New gplib.h library for reusable functions.

--- main.c
+++ main.c
@@ -1,180 +1,16 @@
+/**
+* @author Vijay Kanta
+* @website www.vizcreations.com
+* @abstract Simple console configurator for game Grand Prix 4
+* @license Free software - No guarantee
+* @copyright Not applicable
+* @link https://github.com/vijaykanta/gp4_gfx_config.git
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-#define MAX_PATH 1000
-#define MAX_STRUCT 5000
-#define MAX_LEN 150
-#define MAX_LINE 255
-#define MAX_LINES 100
-#define MAX_VAL 20
-
-char *token;
-char *buff;
-char *value;
-char *comment;
-int val;
-int i;
-char sel;
-//char *token;
-
-char delimiters[] = " .,;|";
-
-typedef struct Configurator {
-	char file_name[256];
-	char *cur_line;
-	char **all_lines;
-	int tot_lines;
-	int cur_sel;
-	FILE *fp;
-	int global;
-	int global_key;
-	char *global_cmt;
-	int res_width;
-	int res_width_key;
-	char *res_width_cmt;
-	int res_height;
-	int res_height_key;
-	char *res_height_cmt;
-	int hw_tnl;
-	int hw_tnl_key;
-	char *hw_tnl_cmt;
-	int wnd_mode;
-	int wnd_mode_key;
-	char *wnd_mode_cmt;
-	int vsync;
-	int vsync_key;
-	char *vsync_cmt;
-	int tex_qlty;
-	int tex_qlty_key;
-	char *tex_qlty_cmt;
-	int bump_map;
-	int bump_map_key;
-	char *bump_map_cmt;
-	int mirrors;
-	int mirrors_key;
-	char *mirrors_cmt;
-	int track_map;
-	int track_map_key;
-	char *track_map_cmt;
-	int env_map;
-	int env_map_key;
-	char *env_map_cmt;
-	int tex_fil_qlty;
-	int tex_fil_qlty_key;
-	char *tex_fil_qlty_cmt;
-	int an_fil_qlty;
-	int an_fil_qlty_key;
-	char *an_fil_qlty_cmt;
-	int shdws;
-	int shdws_key;
-	char *shdws_cmt;
-	int shdw_type;
-	int shdw_type_key;
-	char *shdw_type_cmt;
-	int ext_steer;
-	int ext_steer_key;
-	char *ext_steer_cmt;
-	int heat_haze;
-	int heat_haze_key;
-	char *heat_haze_cmt;
-} CFG;
-
-void strip_newline(char *str, int len) {
-	int i = 0;
-	for(i=0; i<len; i++) {
-		if(str[i] == '\n') {
-			str[i] = '\0';
-			break;
-		}
-	}
-}
-
-void strip_tab(char *str, int len) {
-	int i = 0;
-	for(i=0; i<len; i++) {
-		if(str[i] == '\t') {
-			str[i] = '\0';
-			break;
-		}
-	}
-}
-
-void save_file(CFG *cfg) {
-	printf("Generating new configuration file from buffer..\n");
-	cfg->fp = fopen("f1graphics.cfg", "w");
-	if(!cfg->fp) {
-		printf("Couldn't write file!\n");
-	} else {
-		for(i=0; i<cfg->tot_lines; i++) {
-			fprintf(cfg->fp, "%s\n", cfg->all_lines[i]);
-		}
-		fclose(cfg->fp);
-		printf("New configuration file created successfully!\n");
-	}
-}
-
-void show_file_contents(CFG *cfg) {
-	char *buff;
-	int counter = 0;
-	cfg->fp = fopen(cfg->file_name, "r");
-	//if(sizeof(buff) > 0) free(buff);
-	buff = (char *) malloc(sizeof(char) * MAX_LINE);
-	while(fgets(buff, MAX_LINE, cfg->fp)) {
-		printf("%d: %s", counter, buff);
-		free(buff);
-		buff = (char *) malloc(sizeof(char) * MAX_LINE);
-		++counter;
-	}
-	free(buff);
-}
-
-void get_val_cmt_(CFG *cfg) {
-	if(sizeof(buff) > 0) free(buff);
-	if(sizeof(value) > 0) free(value);
-	if(sizeof(comment) > 0) free(comment);
-
-	buff = (char *) malloc(sizeof(char) * MAX_LINE);
-	value = (char *) malloc(sizeof(char) * MAX_VAL);
-	comment = (char *) malloc(sizeof(char) * MAX_LINE);
-	
-	strcpy(buff, cfg->all_lines[cfg->cur_sel]);
-	token = strtok(buff, ";");
-
-	if(token != NULL) {
-		//value[0] = '\0';
-		strcpy(value, token);
-		strip_tab(value, MAX_VAL);
-	}
-
-	token = strtok(NULL, ";");
-
-	if(token != NULL) {
-		//comment[0] = '\0';
-		strcpy(comment, token);
-		strip_newline(comment, MAX_LINE);
-	}
-
-}
-
-void set_val_cmt_(CFG *cfg) {
-	int last;
-	free(cfg->all_lines[cfg->cur_sel]);
-	cfg->all_lines[cfg->cur_sel] = (char *) malloc(sizeof(char) * MAX_LINE);
-	cfg->all_lines[cfg->cur_sel][0] = '\0';
-
-	free(value);
-	value = (char *) malloc(sizeof(char) * MAX_VAL);
-	//value[0] = '\0';
-
-	sprintf(value, "%d\t\t\t", val);
-	strcat(cfg->all_lines[cfg->cur_sel], value);
-	strcat(cfg->all_lines[cfg->cur_sel], ";");
-	strcat(cfg->all_lines[cfg->cur_sel], comment);
-
-	last = strlen(cfg->all_lines[cfg->cur_sel]) + 1;
-	cfg->all_lines[cfg->cur_sel][last] = '\0';
-}
+#include "gplib.h"
 
 void start_(CFG *cfg) {
 	printf("\nType 't' to edit Texture Filter Quality\n");
@@ -183,14 +19,16 @@ void start_(CFG *cfg) {
 	printf("Type 'q' to edit Texture Quality\n");
 	printf("Type 'a' to edit Anisotropic Filter Quality\n");
 	printf("Type 's' to edit Shadow Type\n");
-	printf("Type 'c' to generate configuration file from buffer\n\n");
+	printf("Type 'c' to generate configuration file from buffer\n");
+	printf("Type 'x' to exit\n\n");
 
 	scanf("%c", &sel);
 	if(sel == 't' || sel == 'T') {
 		cfg->cur_sel = cfg->tex_fil_qlty_key;
 		get_val_cmt_(cfg);
+		show_cur_val(cfg);
 
-		printf("Enter value for Texture Filter Quality (%s default): ", value);
+		printf("Enter value for Texture Filter Quality (%s - current): ", value);
 		scanf("%d", &val);
 		cfg->tex_fil_qlty = val;
 
@@ -203,8 +41,9 @@ void start_(CFG *cfg) {
 	} else if(sel == 'm' || sel == 'M') {
 		cfg->cur_sel = cfg->track_map_key;
 		get_val_cmt_(cfg);
+		show_cur_val(cfg);
 
-		printf("Enter value for Track Map (%s default): ", value);
+		printf("Enter value for Track Map (%s - current): ", value);
 		scanf("%d", &val);
 		cfg->track_map = val;
 
@@ -217,8 +56,9 @@ void start_(CFG *cfg) {
 	} else if(sel == 'v' || sel == 'V') {
 		cfg->cur_sel = cfg->env_map_key;
 		get_val_cmt_(cfg);
+		show_cur_val(cfg);
 
-		printf("Enter value for Environment Map (%s default): ", value);
+		printf("Enter value for Environment Map (%s - current): ", value);
 		scanf("%d", &val);
 		cfg->env_map = val;
 
@@ -231,8 +71,9 @@ void start_(CFG *cfg) {
 	} else if(sel == 'q' || sel == 'Q') {
 		cfg->cur_sel = cfg->tex_qlty_key;
 		get_val_cmt_(cfg);
+		show_cur_val(cfg);
 
-		printf("Enter value for Texture Quality (%s default): ", value);
+		printf("Enter value for Texture Quality (%s - current): ", value);
 		scanf("%d", &val);
 		cfg->tex_qlty = val;
 
@@ -245,8 +86,9 @@ void start_(CFG *cfg) {
 	} else if(sel == 'a' || sel == 'A') {
 		cfg->cur_sel = cfg->an_fil_qlty_key;
 		get_val_cmt_(cfg);
+		show_cur_val(cfg);
 
-		printf("Enter value for Anisotropic Filter Quality (%s default): ", value);
+		printf("Enter value for Anisotropic Filter Quality (%s - current): ", value);
 		scanf("%d", &val);
 		cfg->an_fil_qlty = val;
 
@@ -259,8 +101,9 @@ void start_(CFG *cfg) {
 	} else if(sel == 's' || sel == 'S') {
 		cfg->cur_sel = cfg->shdw_type_key;
 		get_val_cmt_(cfg);
+		show_cur_val(cfg);
 
-		printf("Enter value for Shadow Type (%s default): ", value);
+		printf("Enter value for Shadow Type (%s - current): ", value);
 		scanf("%d", &val);
 		cfg->shdw_type = val;
 
@@ -318,6 +161,8 @@ void free_vals(CFG *cfg) {
 }
 
 void read_file(CFG *cfg) {
+	int num;
+	FILE *handler;
 	printf("Now reading contents of file..\n");
 
 	cfg->tot_lines = 0;
@@ -337,6 +182,17 @@ void read_file(CFG *cfg) {
 		}
 	}
 	fclose(cfg->fp);
+
+	printf("Now creating backup file..\n");
+	handler = fopen("f1graphics.cfg.backup", "w");
+	if(handler) {
+		for(num=0; num<cfg->tot_lines; num++) {
+			fprintf(handler, "%s\n", cfg->all_lines[num]);
+		}
+		fclose(handler);
+		printf("Created backup file successfully..\n");
+	}
+
 	init_vals(cfg);
 }	
 
@@ -378,6 +234,7 @@ int main(int argc, char *argv[], char *env[]) {
 		cfg.tot_lines = 0;
 	}
 
+	if(sizeof(buff) > 0) free(buff);
 	if(sizeof(value) > 0) free(value);
 	if(sizeof(comment) > 0) free(comment);
 	
---
+++ readme.txt
@@ -0,0 +1,33 @@
+About GP4Configurator Console:
+==============================
+
+This configurator is a console based version of the Windows Win32 GUI version planned. It performs very similarly in allowing you to change settings.
+The program is still in it's first days, so expect it to not be extremely user friendly.
+
+
+How it works:
+=============
+
+Just run the program after copying it to your user directory. Example: C:\Users\MyName\GP4ConsoleConfig
+When doing your settings, make sure you do not go beyond or below the settings mentioned in the program.
+After making all necessary changes and generating the configuration file, copy the newly generated file to your GP4 installation directory.
+The program creates a backup file everytime you run it (f1graphics.cfg.backup), so in case of any misconfigurations you can always copy the original backup to your GP4 installation directory.
+
+
+What to know:
+=============
+
+This program is an unofficial program. It is completely free and does not give any sort of guarantee to the user using it. It is under your own discretion to use the program.
+There is no copyright attached, and you can use the program as you like and I do not really mind about it.
+
+
+Community:
+==========
+
+The program is deployed on git for community based contributions - https://github.com/vijaykanta/gp4_gfx_config.git
+The program is also deployed on Aaditas, a website where you can complete your programs - http://www.aaditas.com/topic.php?ac=full&id=79
+The community program is only intended to make you people come up with your own versions.
+
+
+
+~Vijay Kanta. aka Viz aka Vizu. :-)
